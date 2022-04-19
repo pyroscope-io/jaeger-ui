03ab3e5e30b4a7c5e601507c7e699c7c07988464
pavelpashkovsky <pavelpashkovsky@gmail.com>
2022-04-19 10:15:49 +0300

    feat: flamegraph as npm module

---
+++ packages/jaeger-ui/src/components/TracePage/TraceTimelineViewer/SpanDetail/FlameGraph.tsx
@@ -0,0 +1,36 @@
+import React, { useEffect, useState } from 'react';
+import { FlamegraphRenderer } from '@pyroscope/flamegraph';
+import '@pyroscope/flamegraph/dist/index.css';
+
+const FlameGraph = ({ url }: { url: string }) => {
+  const [profile, setProfile] = useState();
+  useEffect(() => {
+    const fetchFlameGraphData = async () => {
+      const query = url.split(/.*?query=(.*?)/);
+      const endpoint = url.match(/(.*)\?query/);
+
+      try {
+        const response = await fetch(
+          `${endpoint ? endpoint[1] : ''}/render?from=now-1h&until=now&query=${
+            query[2]
+          }&max-nodes=1024}&format=json`
+        );
+        const data = await response.json();
+
+        setProfile(data);
+      } catch (error) {
+        console.log('error', error);
+      }
+    };
+
+    fetchFlameGraphData();
+  }, []);
+
+  return (
+    <div style={{ width: '100%', height: 300, overflowY: 'auto' }}>
+      <FlamegraphRenderer profile={profile as any} onlyDisplay="flamegraph" showToolbar={false} />
+    </div>
+  );
+};
+
+export default FlameGraph;
